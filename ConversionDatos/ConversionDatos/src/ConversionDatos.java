34f7dc0d777ecd0a32ef61a8b9f799e16f338bf7
Tamara Morcans <morcans@gmail.com>
2021-07-01 11:26:27 -0500

    Primer commit proyecto Java

---
+++ ConversionDatos/ConversionDatos/src/ConversionDatos.java
@@ -0,0 +1,36 @@
+
+public class ConversionDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* 
+		 * Tipos de datos en Java:
+		 * Númericos: Byte (8bits) ,short (16 bits),int (32 bits),long (64 bits),
+		 * float (Para numeros decimales) ,double
+		 * Booleanos (verdadero,falso): boolean
+		 * Cararcteres: char
+		 * Cadenas: String
+		 * */
+		
+		byte n1=122;
+		byte n2=122;
+		int suma= n1+n2;
+		
+		
+		
+		
+		
+		int numero1 = 1000;
+		long numero2 = numero1;
+		float numero3= numero2;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(suma);
+		
+
+	}
+
+}
